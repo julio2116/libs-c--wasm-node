ebb9fa7a5e62b1a943cc64012258783bb7336524
julio2116 <juliobatista2116@gmail.com>
2025-05-30 12:50:55 -0300

    subindo correcoes na funcao classificar garantindo que seja utilizada a referencia mas, sem riscos de sobrepor a info

--- exercicio2/functions.cpp
+++ exercicio2/functions.cpp
@@ -28,47 +28,28 @@ void filtrarVisitantes(int visitantes, std::vector<std::string> &todos_nomes, st
     std::cout << "Digite a idade minima para filtrar os visitantes:" << std::endl;
     std::cin >> filter;
     std::cout << "Visitantes com idade igual ou superior a " << filter << ":" << std::endl;
+
     for (int j = 0; j < visitantes; j++)
     {
         if (todas_idades.at(j) >= filter)
         {
-            for (int j = 0; j < visitantes; j++)
-            {
-                if (todas_idades.at(j) >= filter)
-                {
-                    std::cout << "- " << todos_nomes.at(j) << std::endl;
-                }
-            }
+            std::cout << "- " << todos_nomes.at(j) << std::endl;
         }
     }
 }
-void classificarIdade(std::vector<int> todas_idades, std::vector<std::string> todos_nomes)
-{
-    std::cout << "Faixas etarias:" << std::endl;
-    std::map<std::string, int> groups;
 
-    for (int i; i < todas_idades.size(); i++)
-    {
-        std::string classification = classificar(todas_idades.at(i));
-        groups[classification] = groups[classification] + 1;
-    }
-    for (auto group : groups)
-    {
-        std::cout << group.first << "s: " << group.second << std::endl;
-    }
-}
 std::string classificar(int idade)
 {
     std::string classificacao;
-    if (idade > 60)
+    if (idade >= 60)
     {
         classificacao = "idoso";
     }
-    else if (idade > 20)
+    else if (idade >= 20)
     {
         classificacao = "adulto";
     }
-    else if (idade > 14)
+    else if (idade >= 14)
     {
         classificacao = "adolescente";
     }
@@ -77,4 +58,19 @@ std::string classificar(int idade)
         classificacao = "crianca";
     }
     return classificacao;
+}
+void classificarIdade(const std::vector<int>& todas_idades, const std::vector<std::string>& todos_nomes)
+{
+    std::cout << "Faixas etarias:" << std::endl;
+    std::map<std::string, int> groups;
+
+    for (int i = 0; i < todas_idades.size(); i++)
+    {
+        std::string classification = classificar(todas_idades.at(i));
+        groups[classification] = groups[classification] + 1;
+    }
+    for (auto group : groups)
+    {
+        std::cout << group.first << "s: " << group.second << std::endl;
+    }
 }
