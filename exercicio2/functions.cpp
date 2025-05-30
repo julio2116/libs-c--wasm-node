dca0710856232d3c1f123670a35692c78cea1555
Julio Cesar Batista da Silva <jcesilva@sfiec.org.br>
2025-05-30 10:22:50 -0300

    adicao no exercicio 2

--- exercicio2/functions.cpp
+++ exercicio2/functions.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 
 void inserirVisitantes(int visitantes, std::vector<std::string> &todos_nomes, std::vector<int> &todas_idades){
     int i = 0;
@@ -24,11 +25,35 @@ void filtrarVisitantes(int visitantes, std::vector<std::string>& todos_nomes, st
     std::cout << "Digite a idade minima para filtrar os visitantes:" << std::endl;
     std::cin >> filter;
     std::cout << "Visitantes com idade igual ou superior a " << filter << ":" << std::endl;
-    for (int j = 0; j < visitantes; j++)
-    {
+    for (int j = 0; j < visitantes; j++){
         if (todas_idades.at(j) >= filter)
         {
             std::cout << "- " << todos_nomes.at(j) << std::endl;
         }
     }
+}
+void classificarIdade(std::vector<int> todas_idades, std::vector<std::string> todos_nomes){
+    std::cout << "Faixas etarias:" << std::endl;
+    std::map<std::string, int> groups;
+
+    for(int i; i < todas_idades.size(); i++){
+        std::string classification = classificar(todas_idades(i));
+        groups[classificar] = groups[classificar] + 1;
+    }
+    for(auto group : groups){
+        std::cout << group.first << "s: " << group.second << std::endl;
+    }
+}
+std::string classificar(int idade){
+    std::string classificacao;
+    if(idade > 60){
+        classificacao = "idoso";
+    } else if(idade > 20){
+        classificacao = "adulto";
+    } else if(idade > 14){
+        classificacao = "adolescente";
+    } else {
+        classificacao = "crianca";
+    }
+    return classificacao;
 }
