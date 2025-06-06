f71420ead191e184a982f6d7e714bfdb8074d34d
julio2116 <juliobatista2116@gmail.com>
2025-06-06 12:18:32 -0300

    Arquivo functions.cpp corrigido

--- exercicio4/functions.cpp
+++ exercicio4/functions.cpp
@@ -4,37 +4,43 @@
 #include <vector>
 #include <map>
 
-void writeFile(const std::string& archive, const int& times){
+void writeFile(const std::string &archive, const int &times){
     std::string nome;
     int idade;
     std::vector<std::pair<std::string, int>> nomesIdades;
     std::ofstream arquivo(archive);
     std::cout << "Insira o nome e em seguida a idade das pessoas:" << std::endl;
-    for (int i = 0; i < times; i++){
-        std::cout << "Insira o nome da pessoa " << i + 1 << std::endl;
-        std::getline(std::cin >> std::ws, nome);
-        std::cout << "Insira a idade de " << nome << std::endl;
-        std::cin >> idade;
-        nomesIdades.push_back({nome, idade});
-    }
-    for(const auto& item : nomesIdades){
-        arquivo << "\"" << item.first << "\"," << item.second << "\n";
+    if (arquivo.is_open()){
+        for (int i = 0; i < times; i++){
+            std::cout << "Insira o nome da pessoa " << i + 1 << std::endl;
+            std::getline(std::cin >> std::ws, nome);
+            std::cout << "Insira a idade de " << nome << std::endl;
+            std::cin >> idade;
+            nomesIdades.push_back({nome, idade});
+        }
+        for (const auto &item : nomesIdades){
+            arquivo << "\"" << item.first << "\"," << item.second << "\n";
+        }
+    } else {
+        std::cout << "Erro no arquivo solicitado!" << std::endl;
     }
     arquivo.close();
 }
-void readFile(const std::string& archive){
+void readFile(const std::string &archive){
     std::string linha;
     int position;
     std::ifstream arquivo(archive);
-    std::map<std::string, int> output;
 
-    while (std::getline(arquivo, linha)){
-        position = linha.find(",");
-        std::string nomeAspas =  linha.substr(0, position);
-        std::string nome = nomeAspas.substr(0, nomeAspas.size() - 2);
-        int idade = stoi(linha.substr(position + 1));
-        std::cout << nomeAspas << " tem " << idade << " anos" << std::endl;
+    if (arquivo.is_open()){
+        while (std::getline(arquivo, linha)){
+            position = linha.find(",");
+            std::string nomeAspas = linha.substr(0, position);
+            std::string nome = nomeAspas.substr(1, nomeAspas.size() - 2);
+            int idade = stoi(linha.substr(position + 1));
+            std::cout << nome << " tem " << idade << " anos" << std::endl;
+        }
+    } else {
+        std::cout << "Erro no arquivo solicitado!" << std::endl;
     }
-
     arquivo.close();
 }
